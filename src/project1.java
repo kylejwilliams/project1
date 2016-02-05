793402c927c792adb706ab031337eaefff84180a
kylejwilliams <kjwkc3@mail.umsl.edu>
2016-02-05 01:50:31 -0600

    Created Node and Problem classes
    - created a problem class to hold information regarding the problem trying to
      be solved. The purpose of this class is to avoid having to pass this
      information along to each function repeatedly; instead, it is all housed
      within a single class. This is frowned upon, so I may look for another
      solution
    - Created a node class to represent the current "object", or node, within
      the decision tree. The initial node is created from the problem, and from
      there all further nodes are produced based on the valid actions taken. A
      stack of these objects will be kept for tracing the steps for getting the
      solution, if one exists.
    - Changed all references of Vector<> objects to ArrayList<>'s instead. This
      was based on implementation problems I was having caused by the vector
      objects. ArrayLists are much simpler to use and, according to several online
      referrals, the "optimal" data structure to use. So far it is working out,
      but I may have to look into a better data structure in the future.
    - Added small methods that were outlined within the pseudocode/implementation
      details, such as makeNode(), which takes in an ArrayList representing the
      current state of the problem and creates a node, and goalTest(), which
      compares a given node with the solution to the problem to check for
      equality. This will also act as the break to the recursive function.
    - Added javadocs-style comments. These may not be the best way to go, but they
      are something I'm wanting to mess around with. All of the parameters to the
      various functions are outlined, but not the actual implementation details.

--- src/project1.java
+++ src/project1.java
@@ -1,5 +1,7 @@
 
-/* 
+import java.util.*;
+
+/** 
  * PSEUDOCODE - Depth-Limited Search
  *
  * function DepthLimitedSearch(problem, limit) returns a solution, or failure/cutoff
@@ -16,9 +18,8 @@
  *             if result = cutoff then cutoffOccured<-true
  *             else if result != failure then return result
  *         if cutoffOccured? then return cutoff else return failure
- */
-
-/*
+ *
+ *
  * IMPLEMENTATION DETAILS
  * 
  * problem      := convert <3, 3, 1> to <0, 0, 0>, using vector addition/subtraction
@@ -37,76 +38,131 @@
  * result       := holds the final solution/failure resulting from recursion
  * actionList   := holds all of the previous performed actions
  * 
- */
-
-import java.util.*;
-
-/**
- * @author kyle
  *
- */
-/**
- * @author kyle
+ * @author Kyle Williams
  *
- */
+ **/
 public class project1 {
 	
 	/**
 	 * @param args command line arguments
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Problem problem = new Problem();
+		int limit = 11;
+		
+		Node result = 
+				DepthLimitedSearch(problem, limit);
+		
+		if (result.state.equals(problem.solution)) {
+			System.out.println("solution found!");
+		}
+		else {
+			System.out.println("failure!");
+		}
 	}
 	
 	
 	/**
-	 * @param initial
-	 * @param solution
-	 * @param limit
-	 * @return
+	 * @param problem	The current problem trying to be solved, as represented
+	 * 					as vectors.The problem contains the initial state and
+	 * 					the solution state, as well as a list of valid actions
+	 * 					that may be performed to reach the solution. 
+	 * @param limit		The maximum depth for the search. The decision tree 
+	 * 					will not search further than this limit. Small values
+	 * 					for the limit may not produce a solution and large 
+	 * 					values may have a long computation time.
+	 * @return			This function returns a recursive call to the depth
+	 * 					limited search. This call will either return the valid 
+	 * 					solution if one exists, or will return a failure 
+	 * 					message
 	 */
-	private static Vector<Integer> DepthLimitedSearch(Vector<Integer> initial, Vector<Integer> solution, int limit) {
-		return RecursiveDLS(initial, initial, solution, limit);
+	private static Node DepthLimitedSearch(Problem problem, int limit) {
+		return RecursiveDLS(makeNode(problem.solution), problem, limit);
 		
 	}
 	
 	/**
-	 * @param node
-	 * @param initial
-	 * @param solution
-	 * @param limit
-	 * @return
+	 * @param node		An object that holds a given state of the problem (a 
+	 * 					intermediate step between the initial state and the '
+	 * 					solution), as well as it's child node, and the action 
+	 * 					that was performed to create said node.
+	 * @param problem	The problem trying to be solved
+	 * @param limit		The maximum depth to search through the tree for a 
+	 * 					solution
+	 * @return			Either the solution to the problem upon success, or
+	 * 					a failure message, to be debugged.
 	 */
-	private static Vector<Integer> RecursiveDLS(Vector<Integer> node, Vector<Integer> initial, Vector<Integer> solution, int limit) {
+	private static Node RecursiveDLS(Node node, Problem problem, int limit) {
 		return node;
 	}
-
+	
+	/**
+	 * @param node		A new node to be created
+	 * @return			The newly created node
+	 */
+	public static Node makeNode(ArrayList<Integer> node) {
+		Node newNode = new Node(node);
+		return newNode;
+	}
 }
-
 
 /**
  * @author kyle
  *
  */
 class Problem {
-	public Vector<Integer> initialState = new Vector<Integer>();
-	public Vector<Integer> solution = new Vector<Integer>();
-	public Vector<Vector<Integer>> actionList = new Vector<Vector<Integer>>();
+	public ArrayList<Integer> initialState = new ArrayList<Integer>();
+	public ArrayList<Integer> solution = new ArrayList<Integer>();
+	public ArrayList<ArrayList<Integer>> actionList = new ArrayList<ArrayList<Integer>>();
 	
-	Problem() {
-		initialState.add(3);
-		initialState.add(3);
-		initialState.add(1);
+
+	public Problem() {
+		initialState.add(3); // number of missionaries
+		initialState.add(3); // number of cannibals
+		initialState.add(1); // boat on wrong side?
 		
-		solution.add(0);
-		solution.add(0);
-		solution.add(0);
+		for (int i = 0; i < 3; i++) {
+			solution.add(0); 
+		}
+		
+		int possibleActions[][] = { 
+				{ 1, 0, 1 }, 
+                { 2, 0, 1 }, 
+                { 0, 1, 1 }, 
+                { 0, 2, 1 }, 
+                { 1, 1, 1 } };
+		
+		ArrayList<Integer> currentAction = new ArrayList<Integer>();
+		for (int y = 0; y < 5; y++) {
+			for (int x = 0; x < 3; x++) {
+				currentAction.add(possibleActions[y][x]);
+			}
+			actionList.add(new ArrayList<Integer>(currentAction));
+			currentAction.clear();
+		}
 	}
 	
-	public boolean goalTest(Vector<Integer> possibleSolution) {
-		if (solution.equals(possibleSolution)) return true;
+	/**
+	 * @param possibleSolution	A given node to be tested for equality with the
+	 * 							problem's solution
+	 * @return
+	 */
+	public boolean goalTest(Node possibleSolution) {
+		if (solution.equals(possibleSolution.state)) return true;
 		else return false;
 	}
 }
 
+/**
+ * @author kyle
+ *
+ */
+class Node {
+	public ArrayList<Integer> state = new ArrayList<Integer>();
+	Node childNode;
+	
+	Node(ArrayList<Integer> node) {
+		state = node;
+	}
+}
