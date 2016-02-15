1c98bc8587613ea014a170589a94b23693dce3f3
KyleJWilliams <kjwkc3@mail.umsl.edu>
2016-02-15 01:46:23 -0600

    Debugging
    Program currently running in a loop because it is not disregarding nodes that
    have already previously been searched.

--- src/project1.java
+++ src/project1.java
@@ -42,11 +42,11 @@ import java.util.*;
  * @author Kyle Williams
  *
  **/
-public class project1 {
+/*public class project1 {
 	
-	/**
+	*//**
 	 * @param args command line arguments
-	 */
+	 *//*
 	public static void main(String[] args) {
 		Problem problem = new Problem();
 		int limit = 11;
@@ -71,7 +71,7 @@ public class project1 {
 	}
 	
 	
-	/**
+	*//**
 	 * @param problem	The current problem trying to be solved, as represented
 	 * 					as vectors.The problem contains the initial state and
 	 * 					the solution state, as well as a list of valid actions
@@ -84,13 +84,13 @@ public class project1 {
 	 * 					limited search. This call will either return the valid 
 	 * 					solution if one exists, or will return a failure 
 	 * 					message
-	 */
+	 *//*
 	private static Node DepthLimitedSearch(Problem problem, int limit) {
-		return RecursiveDLS(makeNode(problem.solution), problem, limit);
+		return RecursiveDLS(makeNode(problem.initialState), problem, limit);
 		
 	}
 	
-	/**
+	*//**
 	 * @param node		An object that holds a given state of the problem (a 
 	 * 					intermediate step between the initial state and the '
 	 * 					solution), as well as it's child node, and the action 
@@ -100,7 +100,7 @@ public class project1 {
 	 * 					solution
 	 * @return			Either the solution to the problem upon success, or
 	 * 					a failure message, to be debugged.
-	 */
+	 *//*
 	private static Node RecursiveDLS(Node node, Problem problem, int limit) {
 		if (problem.goalTest(node)) return node;
 		else if (limit == 0) return new Node(problem.cutoffError);
@@ -108,6 +108,7 @@ public class project1 {
 			boolean cutoffOccured = false;
 			
 			for (ArrayList<Integer> action : problem.validActions(node)) {
+				System.out.print("Current Action: ");
 				System.out.println(action);
 				Node child = makeChildNode(problem, node, action);
 				Node result = RecursiveDLS(child, problem, limit - 1);
@@ -121,23 +122,23 @@ public class project1 {
 		}
 	}
 	
-	/**
+	*//**
 	 * @param node		A new node to be created
 	 * @return			The newly created node
-	 */
+	 *//*
 	public static Node makeNode(ArrayList<Integer> node) {
 		Node newNode = new Node(node);
 		return newNode;
 	}
 	
-	/**
+	*//**
 	 * @param problem 	the problem trying to be solved
 	 * @param node		the parent node that the child is derived from
 	 * @param action	the action to be performed on the parent node to get 
 	 * 					the child node
 	 * @return			A new node that is one level deeper within the decision
 	 * 					tree
-	 */
+	 *//*
 	public static Node makeChildNode(Problem problem, Node node, 
 			ArrayList<Integer> action) {
 		
@@ -148,16 +149,16 @@ public class project1 {
 		}
 		
 		Node tempNode = new Node(tempState);
-		node.childNode = tempNode;
+		node.childrenNodes.add(tempNode);
 
 		return tempNode;
 	}
 }
 
-/**
+*//**
  * @author kyle
  *
- */
+ *//*
 class Problem {
 	public ArrayList<Integer> initialState = new ArrayList<Integer>();
 	public ArrayList<Integer> solution = new ArrayList<Integer>();
@@ -199,22 +200,24 @@ class Problem {
 			failureError.add(-2);
 	}
 	
-	/**
+	*//**
 	 * @param node		A node being evaluated for possible actions to take
 	 * @return			a list of valid actions that can be performed on the 
 	 * 					node
-	 */
+	 *//*
 	public ArrayList<ArrayList<Integer>> validActions(Node node) {
 		ArrayList<ArrayList<Integer>> actions = 
 				new ArrayList<ArrayList<Integer>>();
 		ArrayList<Integer> tempState = new ArrayList<Integer>();
 		
 		for (ArrayList<Integer> action : actionList) {
-			if (action.get(2) == 1) { // boat is on the wrong side
+			System.out.print("Action: ");
+			System.out.println(action);
+			if (node.state.get(2) == 1) { // boat is on the wrong side
 				for (int i = 0; i < action.size(); i++)
 					tempState.add(node.state.get(i) - action.get(i));
 			} 
-			else if (action.get(2) == 0) { // boat is on the right side
+			else if (node.state.get(2) == 0) { // boat is on the right side
 				for (int i = 0; i < action.size(); i++)
 					tempState.add(node.state.get(i) + action.get(i));
 			}
@@ -239,26 +242,199 @@ class Problem {
 		return actions;
 	}
 	
-	/**
+	*//**
 	 * @param possibleSolution	A given node to be tested for equality with the
 	 * 							problem's solution
 	 * @return
-	 */
+	 *//*
 	public boolean goalTest(Node possibleSolution) {
 		if (solution.equals(possibleSolution.state)) return true;
 		else return false;
 	}
 }
 
-/**
+*//**
  * @author kyle
  *
- */
+ *//*
 class Node {
 	public ArrayList<Integer> state = new ArrayList<Integer>();
-	Node childNode;
+	public ArrayList<Node> childrenNodes = new ArrayList<Node>();
 	
 	Node(ArrayList<Integer> node) {
 		state = node;
 	}
+}*/
+
+class project1 {
+	
+	public static void main(String[] args) {
+		Problem problem = new Problem();
+		
+		Node result = iterativeDeepeningDepthFirstSearch(problem);
+		System.out.println("Result: " + result);
+		
+		// DEBUGGING
+		//ArrayList<Integer> state = new ArrayList<>();
+		//state.add(3);
+		//state.add(3);
+		//state.add(1);
+		//Node test = makeNode(state);
+		//makeChildrenNodes(test);
+		
+		//for (Node node : test.children) {
+		//	System.out.println(node.state);
+		//}
+		
+		//System.out.println(isActionValid(action));
+	}
+	
+	public static Node iterativeDeepeningDepthFirstSearch(Problem problem) {
+		int depth = 0;
+		Node result = new Node();
+		
+		
+		while (true) {
+			depth++;
+			result = depthLimitedSearch(problem, problem.initialState, depth);
+			
+			if (result != null)
+				return result;
+		}
+	}
+	
+	public static Node depthLimitedSearch(Problem problem, Node node, int depth) {
+		if (node.state == problem.solution.state) {
+			return node;
+		}
+		else if (depth > 0) {
+			makeChildrenNodes(problem, node);
+			//System.out.println("depth: " + depth + " current Node: " + node.state + " visited nodes: " + problem.visitedNodes);
+			Node result;
+			for (Node child : node.children) {
+				problem.visitedNodes.add(child.state);
+				//if (problem.visitedNodes.contains(child.state)) break;
+				result = depthLimitedSearch(problem, child, depth - 1);
+				
+				if (result != null)
+					return result;
+			}
+		}
+		
+		return null;
+	}
+	
+	public static Node makeNode(ArrayList<Integer> state) {
+		Node node = new Node();
+		node.state = state;
+		return node;
+	}
+	
+	@SuppressWarnings("boxing")
+	public static void makeChildrenNodes(Problem problem, Node node) {
+		ArrayList<ArrayList<Integer>> possibleActions = new ArrayList<>();
+		ArrayList<Integer> action = new ArrayList<>();
+		
+		// <1, 0, 1>
+		action.add(1);
+		action.add(0);
+		action.add(1);
+		possibleActions.add(action);
+		action = new ArrayList<>();
+		
+		// <2, 0, 1>
+		action.add(2);
+		action.add(0);
+		action.add(1);
+		possibleActions.add(action);
+		action = new ArrayList<>();
+		
+		// <0, 1, 1>
+		action.add(0);
+		action.add(1);
+		action.add(1);
+		possibleActions.add(action);
+		action = new ArrayList<>();
+		
+		// <0, 2, 1>
+		action.add(0);
+		action.add(2);
+		action.add(1);
+		possibleActions.add(action);
+		action = new ArrayList<>();
+		
+		// <1, 1, 1>
+		action.add(1);
+		action.add(1);
+		action.add(1);
+		possibleActions.add(action);
+		action = new ArrayList<>();
+		
+		if (node.state.get(2) == 1) { // boat is on the wrong side
+			for (int i = 0; i < possibleActions.size(); i++) {
+				for (int j = 0; j < possibleActions.get(i).size(); j++) {
+					possibleActions.get(i).set(j, (-1)*possibleActions.get(i).get(j));
+				}
+			}
+		}
+		
+		for (ArrayList<Integer> currentAction : possibleActions) {
+			for (int i = 0; i < currentAction.size(); i++) {
+				action.add(currentAction.get(i) + node.state.get(i));
+			}
+			
+			if (isActionValid(action, problem.visitedNodes))
+				node.children.add(makeNode(action));
+			
+			action = new ArrayList<>();
+		}
+		
+	}
+	
+	@SuppressWarnings("boxing")
+	public static boolean isActionValid(ArrayList<Integer> action, ArrayList<ArrayList<Integer>> visitedNodes) {
+		for (ArrayList<Integer> test : visitedNodes) {
+			//System.out.println("Action: " + action);
+			//System.out.println("Visited Nodes: " + visitedNodes);
+			//System.out.println(action == test);
+			
+			if (action == test) {
+				System.out.println("invalid action");
+				return false;
+			}
+		}
+		if (action.get(0) >= action.get(1) && 
+				action.get(0) >= 0 &&
+				action.get(0) <= 3 &&
+				action.get(1) >= 0 &&
+				action.get(1) <= 3 &&
+				action.get(2) >= 0 &&
+				action.get(2) <= 1) {	
+			return true;
+		}
+		return false;
+	}
+}
+
+class Node {
+	public ArrayList<Integer> state = new ArrayList<>();
+	public ArrayList<Node> children = new ArrayList<>();
+}
+
+class Problem {
+	public Node initialState = new Node();
+	public Node solution = new Node();
+	public ArrayList<ArrayList<Integer>> visitedNodes = new ArrayList<>();
+	
+	@SuppressWarnings("boxing")
+	public Problem() {
+		this.initialState.state.add(3);
+		this.initialState.state.add(3);
+		this.initialState.state.add(1);
+		
+		this.solution.state.add(0);
+		this.solution.state.add(0);
+		this.solution.state.add(0);
+	}
+	
 }
